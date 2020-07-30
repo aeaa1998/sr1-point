7a052724326674686fab65764251de53c8639b79
Augusto Alonso <augustoalonso98@gmail.com>
2020-07-29 19:57:54 -0600

    models

--- Renderer.py
+++ Renderer.py
@@ -1,4 +1,5 @@
 import struct
+from obj import Obj
 import math
 
 
@@ -255,6 +256,26 @@ class Render(object):
                         if any(i.y <= indexY for i in iterableLinesX) and any(i.y >= indexY for i in iterableLinesX):
                             if any(i.x <= indexX for i in iterableLinesY) and any(i.x >= indexX for i in iterableLinesY):
                                 self.point(indexX, indexY)
+
+    def load(self, filename, translate, scale):
+        model = Obj(filename)
+
+        for face in model.faces:
+            vcount = len(face)
+
+            for j in range(vcount):
+                f1 = face[j][0]
+                f2 = face[(j + 1) % vcount][0]
+
+                v1 = model.vertices[f1 - 1]
+                v2 = model.vertices[f2 - 1]
+
+                x1 = round((v1[0] + translate[0]) * scale[0])
+                y1 = round((v1[1] + translate[1]) * scale[1])
+                x2 = round((v2[0] + translate[0]) * scale[0])
+                y2 = round((v2[1] + translate[1]) * scale[1])
+
+                self.line(x1, y1, x2, y2, False)
 
 
 
